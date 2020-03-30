2506f20ea3a67fb366ebc3a899313d5e0a759e9e
HEINZLE Nino <nhe8171@students.fhv.at>
2020-03-30 15:45:53 +0200

    Update gradle modules for a working frame

--- src/main/java/at/fhv/teamb/symphoniacus/Main.java
+++ src/main/java/at/fhv/teamb/symphoniacus/Main.java
@@ -1,24 +1,49 @@
 package at.fhv.teamb.symphoniacus;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+
+//import static javafx.application.ConditionalFeature.FXML;
 
 public class Main extends Application {
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
     @Override
     public void start(Stage stage) {
+
+
+        /*
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        /*
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
         stage.show();
+        */
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/basic_frame.fxml"));
+
+            Scene scene = new Scene(root);
+            stage.setTitle("Mailer");
+            stage.setScene(scene);
+            stage.show();
+
+        }catch (IOException e){
+            e.fillInStackTrace();
+        }
+
     }
 }
