8c5548831d1377d17a030f950f7024e23b4e335a
Theresa Gierer <theresa.gierer@students.fhv.at>
2020-04-06 14:42:27 +0200

    Enhance POJOS

---
+++ src/main/java/at/fhv/teamb/symphoniacus/presentation/CalendarController.java
@@ -0,0 +1,9 @@
+package at.fhv.teamb.symphoniacus.presentation;
+
+import com.calendarfx.view.CalendarView;
+import javafx.fxml.FXML;
+
+public class CalendarController {
+    @FXML
+    public CalendarView calendarView;
+}
---
+++ src/main/java/at/fhv/teamb/symphoniacus/presentation/TabPaneController.java
@@ -0,0 +1,9 @@
+package at.fhv.teamb.symphoniacus.presentation;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class TabPaneController {
+    @FXML
+    public AnchorPane calendar;
+}
