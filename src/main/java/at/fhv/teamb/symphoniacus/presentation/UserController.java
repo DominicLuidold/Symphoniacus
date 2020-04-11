7d1a4a89793ab338690416fb25e208d059592f0d
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-04-11 14:05:02 +0200

    add log4j2 logger

--- src/main/java/at/fhv/teamb/symphoniacus/presentation/UserController.java
+++ src/main/java/at/fhv/teamb/symphoniacus/presentation/UserController.java
@@ -4,8 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 import javafx.scene.text.TextFlow;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class UserController {
+
+    private static final Logger LOG = LogManager.getLogger(UserController.class);
+
     @FXML
     private TextFlow txtFlowSection;
 
@@ -14,6 +19,6 @@ public class UserController {
 
     @FXML
     void handleUserLogout(ActionEvent event) {
-        System.out.println("test");
+        LOG.info("Test");
     }
 }
