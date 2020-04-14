da1231725ac2e3e08123e3f78b5b5f5ba1ed8801
Dominic Luidold <dominic.luidold@students.fhv.at>
2020-04-14 12:42:06 +0200

    Streamline tests

--- src/test/java/at/fhv/teamb/symphoniacus/persistence/dao/DutyDaoTest.java
+++ src/test/java/at/fhv/teamb/symphoniacus/persistence/dao/DutyDaoTest.java
@@ -1,45 +1,53 @@
 package at.fhv.teamb.symphoniacus.persistence.dao;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import at.fhv.teamb.symphoniacus.persistence.model.Duty;
 import at.fhv.teamb.symphoniacus.persistence.model.Section;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class DutyDaoTest {
+class DutyDaoTest {
     private static final Logger LOG = LogManager.getLogger(DutyDaoTest.class);
     private DutyDao dao;
 
     @BeforeAll
-    public void init() {
+    void initialize() {
         this.dao = new DutyDao();
     }
 
     @Test
-    public void testFindAllDutiesForWeek_shouldReturnNotNull() {
+    void testFindAllDutiesForWeek_ShouldReturnNotNull() {
+        // When
         List<Duty> list = this.dao.findAllInRange(
             LocalDateTime.of(2020, 3, 30, 0, 0, 0),
             LocalDateTime.of(2020, 4, 5, 0, 0, 0)
         );
 
-        Assertions.assertTrue(list != null);
+        // Then
+        assertNotNull(list);
     }
 
     @Test
-    public void findAllInRangeWithSection_shouldReturnNotNull() {
+    void findAllInRangeWithSection_ShouldReturnNotNull() {
+        // Given
         Section s = new Section();
         s.setSectionId(1);
+
+        // When
         List<Duty> list = this.dao.findAllInRangeWithSection(s,
             LocalDateTime.of(2020, 5, 1, 0, 0, 0),
             LocalDateTime.of(2020, 5, 1, 14, 0, 0),
             true, false, false);
+
+        // Then
         LOG.debug("Result size? " + list.size());
-        Assertions.assertTrue(list != null);
+        assertNotNull(list);
     }
 }
--- src/test/java/at/fhv/teamb/symphoniacus/persistence/dao/MusicianDaoTest.java
+++ src/test/java/at/fhv/teamb/symphoniacus/persistence/dao/MusicianDaoTest.java
@@ -1,33 +1,32 @@
 package at.fhv.teamb.symphoniacus.persistence.dao;
 
-import at.fhv.teamb.symphoniacus.persistence.model.Duty;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import at.fhv.teamb.symphoniacus.persistence.model.Musician;
-import at.fhv.teamb.symphoniacus.persistence.model.Section;
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class MusicianDaoTest {
+class MusicianDaoTest {
     private static final Logger LOG = LogManager.getLogger(MusicianDaoTest.class);
     private MusicianDao dao;
 
     @BeforeAll
-    public void init() {
+    void initialize() {
         this.dao = new MusicianDao();
     }
 
     @Test
-    public void testFindMusician_shouldReturnAMusician() {
+    void testFindMusician_ShouldReturnAMusician() {
+        // When
         Optional<Musician> m = this.dao.find(1);
-        Assertions.assertTrue(m.isPresent());
+
+        // Then
+        assertTrue(m.isPresent());
         LOG.debug(m.get());
     }
-
 }
