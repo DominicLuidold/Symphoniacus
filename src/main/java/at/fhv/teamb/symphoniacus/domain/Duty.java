9680f2dc3b8133cefb015a6cc37fc217741ea9d8
Dominic Luidold <dominic.luidold@students.fhv.at>
2020-04-18 18:15:29 +0200

    Prepare domain objects

--- src/main/java/at/fhv/teamb/symphoniacus/domain/Duty.java
+++ src/main/java/at/fhv/teamb/symphoniacus/domain/Duty.java
@@ -1,4 +1,21 @@
 package at.fhv.teamb.symphoniacus.domain;
 
+import at.fhv.teamb.symphoniacus.persistence.model.DutyEntity;
+
 public class Duty {
+    private DutyEntity entity;
+
+    public Duty(DutyEntity entity) {
+        this.entity = entity;
+    }
+
+    // TODO - Domain logic
+
+    public DutyEntity getEntity() {
+        return this.entity;
+    }
+
+    public void setEntity(DutyEntity entity) {
+        this.entity = entity;
+    }
 }
