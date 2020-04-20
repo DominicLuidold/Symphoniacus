399b08b0b804357e8dbc8bc3077a8e0f7fdd6803
Dominic Luidold <dominic.luidold@students.fhv.at>
2020-04-20 21:44:26 +0200

    Add implementation for "getInstrumentationDetails"

--- src/main/java/at/fhv/teamb/symphoniacus/application/DutyScheduleManager.java
+++ src/main/java/at/fhv/teamb/symphoniacus/application/DutyScheduleManager.java
@@ -5,12 +5,48 @@ import at.fhv.teamb.symphoniacus.domain.Duty;
 import at.fhv.teamb.symphoniacus.domain.DutyPosition;
 import at.fhv.teamb.symphoniacus.domain.Musician;
 import at.fhv.teamb.symphoniacus.domain.Section;
+import at.fhv.teamb.symphoniacus.persistence.dao.DutyPositionDao;
+import at.fhv.teamb.symphoniacus.persistence.model.DutyPositionEntity;
+import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * This class is responsible for obtaining and handling all instances of actual instrumentations
+ * for given {@link Duty} objects.
+ *
+ * @author Dominic Luidold
+ */
 public class DutyScheduleManager {
+    private DutyPositionDao dutyPositionDao;
 
+    public DutyScheduleManager() {
+        this.dutyPositionDao = new DutyPositionDao();
+    }
+
+    /**
+     * Returns a {@link ActualSectionInstrumentation} domain object containing all available
+     * information about the actual section instrumentation for the given {@link Duty} and
+     * {@link Section}.
+     *
+     * @param duty    The duty to use
+     * @param section The section to use
+     * @return A domain object containing all instrumentation information
+     */
     public ActualSectionInstrumentation getInstrumentationDetails(Duty duty, Section section) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        // Get all DutyPosition entities from database
+        List<DutyPositionEntity> dutyPositionEntities =
+            this.dutyPositionDao.findCorrespondingPositions(duty.getEntity(), section.getEntity());
+
+        // Create DutyPosition domain objects
+        List<DutyPosition> dutyPositions = new LinkedList<>();
+        for (DutyPositionEntity dutyPosition : dutyPositionEntities) {
+            dutyPositions.add(new DutyPosition(dutyPosition));
+        }
+
+        // Fill Duty with available information
+        Duty dutyWithInformation = new Duty(duty.getEntity(), dutyPositions);
+
+        return new ActualSectionInstrumentation(dutyWithInformation);
     }
 
     public List<Musician> getMusiciansAvailableForPosition(DutyPosition position) {
@@ -18,6 +54,10 @@ public class DutyScheduleManager {
     }
 
     public void setMusicianForPosition(Musician musician, DutyPosition position) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void unsetMusicianForPosition(Musician musician, DutyPosition position) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
