fd8cbcde537df92459cb072bf2d47a3e691828ef
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-04-20 21:07:16 +0200

    mock getMusiciansAvailableForPosition

--- src/main/java/at/fhv/teamb/symphoniacus/application/DutyScheduleManager.java
+++ src/main/java/at/fhv/teamb/symphoniacus/application/DutyScheduleManager.java
@@ -2,10 +2,24 @@ package at.fhv.teamb.symphoniacus.application;
 
 import at.fhv.teamb.symphoniacus.domain.ActualSectionInstrumentation;
 import at.fhv.teamb.symphoniacus.domain.Duty;
+import at.fhv.teamb.symphoniacus.persistence.model.DutyPosition;
+import at.fhv.teamb.symphoniacus.persistence.model.Musician;
 import at.fhv.teamb.symphoniacus.persistence.model.Section;
+import java.util.LinkedList;
+import java.util.List;
 
 public class DutyScheduleManager {
+
     public ActualSectionInstrumentation getInstrumentationDetails(Duty duty, Section section) {
         return null;
     }
+
+    public List<Musician> getMusiciansAvailableForPosition(
+        DutyPosition dp,
+        boolean withRequests
+    ) {
+        return new LinkedList<>();
+    }
+
+
 }
