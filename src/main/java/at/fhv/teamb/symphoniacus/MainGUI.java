9251f3be19143df6b6b0abf10522ca47c9c0690a
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-04-26 14:35:04 +0200

    add Symphoniacus icon

--- src/main/java/at/fhv/teamb/symphoniacus/MainGUI.java
+++ src/main/java/at/fhv/teamb/symphoniacus/MainGUI.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -31,6 +32,11 @@ public class MainGUI extends Application {
             stage.setScene(scene);
             stage.setMinHeight(768);
             stage.setMinWidth(1366);
+            stage.getIcons().add(
+                new Image(
+                    "images/icon.png"
+                )
+            );
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
