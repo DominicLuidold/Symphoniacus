4f32073e211127a4c88c5357a56d13488141f2b7
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-04-27 02:06:10 +0200

    try to fix popup

--- src/main/java/at/fhv/teamb/symphoniacus/presentation/DutyScheduleController.java
+++ src/main/java/at/fhv/teamb/symphoniacus/presentation/DutyScheduleController.java
@@ -172,27 +172,37 @@ public class DutyScheduleController implements Initializable, Controllable {
         this.musicianTableWithRequests.setOnMouseClicked((MouseEvent event) -> {
             // add selected item click listener
             if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                alert.setTitle("Musician has Negative Duty Wish");
-                alert.setContentText("Really schedule Musician with Negative Wish?");
-                ButtonType okButton = new ButtonType("Yes", ButtonBar.ButtonData.YES);
-                ButtonType noButton = new ButtonType("No", ButtonBar.ButtonData.NO);
                 MusicianTableModel mtm =
                     this.musicianTableWithRequests.getSelectionModel().getSelectedItem();
-                alert.getButtonTypes().setAll(okButton, noButton);
-                alert.showAndWait().ifPresent(type -> {
-                    System.out.println(type);
-                    if (type.equals(okButton)) {
-                        this.addMusicianToPosition(
-                            this.actualSectionInstrumentation,
-                            mtm.getMusician(),
-                            this.selectedDutyPosition
-                        );
-                        alert.close();
-                    } else if (type.equals(noButton)) {
-                        alert.close();
-                    }
-                });
+                if (!mtm.isWishPositive()) {
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                    alert.setTitle("Musician has Negative Duty Wish");
+                    alert.setContentText("Really schedule Musician with Negative Wish?");
+                    ButtonType okButton = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+                    ButtonType noButton = new ButtonType("No", ButtonBar.ButtonData.NO);
+
+                    alert.getButtonTypes().setAll(okButton, noButton);
+                    alert.showAndWait().ifPresent(type -> {
+                        System.out.println(type);
+                        if (type.equals(okButton)) {
+                            this.addMusicianToPosition(
+                                this.actualSectionInstrumentation,
+                                mtm.getMusician(),
+                                this.selectedDutyPosition
+                            );
+                            alert.close();
+                        } else if (type.equals(noButton)) {
+                            alert.close();
+                        }
+                    });
+                } else {
+                    this.addMusicianToPosition(
+                        this.actualSectionInstrumentation,
+                        mtm.getMusician(),
+                        this.selectedDutyPosition
+                    );
+                }
+
             }
         });
 
@@ -232,25 +242,36 @@ public class DutyScheduleController implements Initializable, Controllable {
                 "Schedule",
                 (MusicianTableModel mtm) -> {
                     LOG.debug("Schedule btn with requests has been pressed");
-                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                    alert.setTitle("Musician has Negative Duty Wish");
-                    alert.setContentText("Really schedule Musician with Negative Wish?");
-                    ButtonType okButton = new ButtonType("Yes", ButtonBar.ButtonData.YES);
-                    ButtonType noButton = new ButtonType("No", ButtonBar.ButtonData.NO);
-                    alert.getButtonTypes().setAll(okButton, noButton);
-                    alert.showAndWait().ifPresent(type -> {
-                        System.out.println(type);
-                        if (type.equals(okButton)) {
-                            this.addMusicianToPosition(
-                                this.actualSectionInstrumentation,
-                                mtm.getMusician(),
-                                this.selectedDutyPosition
-                            );
-                            alert.close();
-                        } else if (type.equals(noButton)) {
-                            alert.close();
-                        }
-                    });
+
+                    if (!mtm.isWishPositive()) {
+                        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                        alert.setTitle("Musician has Negative Duty Wish");
+                        alert.setContentText("Really schedule Musician with Negative Wish?");
+                        ButtonType okButton = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+                        ButtonType noButton = new ButtonType("No", ButtonBar.ButtonData.NO);
+                        alert.getButtonTypes().setAll(okButton, noButton);
+
+                        alert.showAndWait().ifPresent(type -> {
+                            System.out.println(type);
+                            if (type.equals(okButton)) {
+                                this.addMusicianToPosition(
+                                    this.actualSectionInstrumentation,
+                                    mtm.getMusician(),
+                                    this.selectedDutyPosition
+                                );
+                                alert.close();
+                            } else if (type.equals(noButton)) {
+                                alert.close();
+                            }
+                        });
+                    } else {
+                        this.addMusicianToPosition(
+                            this.actualSectionInstrumentation,
+                            mtm.getMusician(),
+                            this.selectedDutyPosition
+                        );
+                    }
+
 
                     return mtm;
                 }
