7d238f00e2e74ebb00d9996e808cbefae07ee8d2
1b0t <ibot@c0re.at>
2020-04-30 20:57:21 +0200

    Maincontroller sets the right User

---
+++ src/main/java/at/fhv/teamb/symphoniacus/persistence/dao/AdministrativeAssistantDao.java
@@ -0,0 +1,39 @@
+package at.fhv.teamb.symphoniacus.persistence.dao;
+
+import at.fhv.teamb.symphoniacus.persistence.BaseDao;
+import at.fhv.teamb.symphoniacus.persistence.model.AdministrativeAssistantEntity;
+import java.util.Optional;
+
+/**
+ * DAO for AdministrativeAssistants.
+ *
+ * @author Tobias Moser
+ */
+public class AdministrativeAssistantDao extends BaseDao<AdministrativeAssistantEntity> {
+    /**
+     * Finds a {@link AdministrativeAssistantEntity} by its key.
+     *
+     * @param key The key of the AdministrativeAssistant
+     * @return The AdministrativeAssistant that is looked for
+     */
+    @Override
+    public Optional<AdministrativeAssistantEntity> find(Integer key) {
+        return this.find(AdministrativeAssistantEntity.class, key);
+    }
+
+    @Override
+    public Optional<AdministrativeAssistantEntity> persist(AdministrativeAssistantEntity elem) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<AdministrativeAssistantEntity> update(AdministrativeAssistantEntity elem) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Boolean remove(AdministrativeAssistantEntity elem) {
+        return null;
+    }
+
+}
--- src/main/java/at/fhv/teamb/symphoniacus/presentation/MainController.java
+++ src/main/java/at/fhv/teamb/symphoniacus/presentation/MainController.java
@@ -1,10 +1,21 @@
 package at.fhv.teamb.symphoniacus.presentation;
 
+import at.fhv.teamb.symphoniacus.application.MusicianManager;
+import at.fhv.teamb.symphoniacus.application.type.DomainUserType;
+import at.fhv.teamb.symphoniacus.domain.AdministrativeAssistant;
+import at.fhv.teamb.symphoniacus.domain.Musician;
 import at.fhv.teamb.symphoniacus.domain.User;
+import at.fhv.teamb.symphoniacus.persistence.dao.AdministrativeAssistantDao;
+import at.fhv.teamb.symphoniacus.persistence.model.AdministrativeAssistantEntity;
+import at.fhv.teamb.symphoniacus.persistence.model.MusicianEntity;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
-public class MainController {
+public class MainController implements Initializable {
     @FXML
     public AnchorPane userHeaderMenu;
 
@@ -12,8 +23,38 @@ public class MainController {
     public AnchorPane tabPane;
 
     private User currentUser;
+    private Musician currentMusician;
+    private AdministrativeAssistant currentAssistant;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    /**
+     * Finds out if the currently logged-in user is a {@link Musician} or an
+     * {@link AdministrativeAssistant} and sets his Domainobject as attribute in this class.
+     * @param user Current login user
+     */
     public void setLoginUser(User user) {
         this.currentUser = user;
+
+        if (this.currentUser.getUserEntity().getType().equals(DomainUserType.DOMAIN_MUSICIAN)) {
+            MusicianManager mm = new MusicianManager();
+            Optional<MusicianEntity> musician = mm.loadMusician(this.currentUser.getUserEntity());
+            if (musician.isPresent()) {
+                this.currentMusician = new Musician(musician.get());
+            }
+        }
+        if (this.currentUser.getUserEntity().getType()
+            .equals(DomainUserType.DOMAIN_ADMINISTRATIVEASSISTANT)) {
+            AdministrativeAssistantDao aad = new AdministrativeAssistantDao();
+            Optional<AdministrativeAssistantEntity> administrativeAssistantEntity =
+                aad.find(this.currentUser.getUserEntity().getUserId());
+            if (administrativeAssistantEntity.isPresent()) {
+                this.currentAssistant =
+                    new AdministrativeAssistant(administrativeAssistantEntity.get());
+            }
+        }
     }
 }
