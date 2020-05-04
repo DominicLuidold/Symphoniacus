ee8c54d1a503205d78ad7437cd9d7e3b788292eb
Theresa Gierer <theresa.gierer@students.fhv.at>
2020-05-04 17:08:54 +0200

    WIP on newDutyEntry.fxml

--- src/main/java/at/fhv/teamb/symphoniacus/presentation/NewDutyEntryController.java
+++ src/main/java/at/fhv/teamb/symphoniacus/presentation/NewDutyEntryController.java
@@ -2,9 +2,12 @@ package at.fhv.teamb.symphoniacus.presentation;
 
 import at.fhv.teamb.symphoniacus.application.LoginManager;
 import at.fhv.teamb.symphoniacus.domain.User;
+import at.fhv.teamb.symphoniacus.persistence.model.DutyCategoryEntity;
+import at.fhv.teamb.symphoniacus.persistence.model.SeriesOfPerformancesEntity;
 import at.fhv.teamb.symphoniacus.presentation.internal.AlertHelper;
 import at.fhv.teamb.symphoniacus.presentation.internal.Parentable;
 import at.fhv.teamb.symphoniacus.presentation.internal.tasks.LoginTask;
+import com.jfoenix.controls.JFXDatePicker;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Locale;
@@ -17,6 +20,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -33,6 +38,12 @@ import org.controlsfx.validation.Validator;
 import org.controlsfx.validation.decoration.StyleClassValidationDecoration;
 import org.controlsfx.validation.decoration.ValidationDecoration;
 
+/**
+ * GUI Controller responsible for creating a new Duty Entry.
+ *
+ * @author Theresa Gierer
+ */
+
 public class NewDutyEntryController implements Initializable {
 
     private static final Logger LOG = LogManager.getLogger(NewDutyEntryController.class);
@@ -42,10 +53,71 @@ public class NewDutyEntryController implements Initializable {
 
     private ResourceBundle resources;
 
+    @FXML
+    private TextField dutyNameInput;
+
+    @FXML
+    private ComboBox<SeriesOfPerformancesEntity> seriesOfPerformancesSelect;
+
+    @FXML
+    private Button seriesOfPerformancesNewBtn;
+
+    @FXML
+    private ComboBox<DutyCategoryEntity> dutyCategorySelect;
+
+    @FXML
+    private TextField dutyPointsInput;
+
+    @FXML
+    private JFXDatePicker dutyStartDateInput;
+
+    @FXML
+    private TextField dutyStartTimeInput;
+
+    @FXML
+    private DatePicker dutyEndDateInput;
+
+    @FXML
+    private TextField dutyEndTimeInput;
+
+    @FXML
+    private TextField dutyPlaceInput;
+
+    private ValidationSupport validationSupport = new ValidationSupport();
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.resources = resources;
+
+
+        ValidationDecoration cssDecorator = new StyleClassValidationDecoration(
+            "error",
+            "warning"
+        );
+        this.validationSupport.setValidationDecorator(cssDecorator);
+
+        this.validationSupport.registerValidator(this.dutyNameInput,
+            Validator.createEmptyValidator(resources.getString(
+                "tab.duty.new.entry.error.namemissing"
+                )
+            )
+        );
+
+        //TODO: Discussion with Team whether SOP is needed or not.
+        /*
+        name
+        sop
+        cat
+        points
+        date
+        time
+        place
+         */
+
+
+
+
     }
 
 
