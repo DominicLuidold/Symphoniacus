d0dbe945a45efa8e67fb6ee9c625de6715894384
Theresa Gierer <theresa.gierer@students.fhv.at>
2020-05-14 23:47:32 +0200

    WIP Testcommit

---
+++ src/main/java/at/fhv/teamb/symphoniacus/persistence/dao/interfaces/ISeriesOfPerformancesDao.java
@@ -0,0 +1,15 @@
+package at.fhv.teamb.symphoniacus.persistence.dao.interfaces;
+
+import at.fhv.teamb.symphoniacus.persistence.Dao;
+import at.fhv.teamb.symphoniacus.persistence.model.SeriesOfPerformancesEntity;
+import at.fhv.teamb.symphoniacus.persistence.model.interfaces.ISeriesOfPerformancesEntity;
+import java.util.List;
+
+public interface ISeriesOfPerformancesDao extends Dao<ISeriesOfPerformancesEntity> {
+    /**
+     * Returns all {@link SeriesOfPerformancesEntity} objects.
+     *
+     * @return A List of series of performances entities
+     */
+    List<ISeriesOfPerformancesEntity> getAll();
+}
