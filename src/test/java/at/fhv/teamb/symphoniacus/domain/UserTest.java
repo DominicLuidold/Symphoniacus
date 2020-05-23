8866b7307fefac92f8ac003307ddf7008905bb17
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-05-23 16:03:34 +0200

    add UserTest

---
+++ src/test/java/at/fhv/teamb/symphoniacus/domain/UserTest.java
@@ -0,0 +1,62 @@
+package at.fhv.teamb.symphoniacus.domain;
+
+import at.fhv.teamb.symphoniacus.application.type.DomainUserType;
+import at.fhv.teamb.symphoniacus.persistence.model.UserEntity;
+import at.fhv.teamb.symphoniacus.persistence.model.interfaces.IUserEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+/**
+ * Tests for the {@link User} domain class.
+ *
+ * @author Valentin Goronjic
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserTest {
+
+    private IUserEntity userEntity;
+    private User user;
+
+    /**
+     * Initial setup for each test.
+     */
+    @BeforeAll
+    public void setUp() {
+        IUserEntity userEntity = new UserEntity();
+        userEntity.setFirstName("Testi");
+        userEntity.setLastName("Testnachname");
+        this.userEntity = userEntity;
+
+        this.user = new User(userEntity);
+        this.user.setType(DomainUserType.DOMAIN_MUSICIAN);
+    }
+
+    @Test
+    public void testGetFullName_shouldReturnFullName() {
+        Assertions.assertEquals(
+            "Testi Testnachname",
+            this.user.getFullName(),
+            "Full Name should be valid"
+        );
+    }
+
+    @Test
+    public void testGetType_shouldReturnType() {
+        Assertions.assertEquals(
+            DomainUserType.DOMAIN_MUSICIAN,
+            this.user.getType(),
+            "Full Name should be valid"
+        );
+    }
+
+    @Test
+    public void testGetEntity_shouldReturnEntity() {
+        Assertions.assertEquals(
+            this.userEntity,
+            this.user.getUserEntity(),
+            "Full Name should be valid"
+        );
+    }
+}
