f855e169ccf7b4c7689b917a779d644406175b8c
Theresa Gierer <theresa.gierer@students.fhv.at>
2020-05-30 18:35:11 +0200

    Create Enum Class WishStatus

---
+++ src/main/java/at/fhv/teamb/symphoniacus/application/dto/wishdtos/WishStatus.java
@@ -0,0 +1,12 @@
+package at.fhv.teamb.symphoniacus.application.dto.wishdtos;
+
+/**
+ * Type class for {@link WishStatus}.
+ *
+ * @author Theresa Gierer
+ */
+public enum WishStatus {
+    REVIEW,
+    APPROVED,
+    REJECTED
+}
